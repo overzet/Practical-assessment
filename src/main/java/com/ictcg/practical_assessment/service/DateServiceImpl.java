cd796cb22754d924e23f1efa4ab35937562763e6
Overzet <overzet@gmail.com>
2019-04-29 12:03:01 +0200

    Unsupported field: AmPmOfDay

--- src/main/java/com/ictcg/practical_assessment/service/DateServiceImpl.java
+++ src/main/java/com/ictcg/practical_assessment/service/DateServiceImpl.java
@@ -15,6 +15,9 @@ public class DateServiceImpl implements DateService {
 
         DateModel dateModel = new DateModel();
 
+        dateModel.setLocalDate(LocalDate.now());
+        dateModel.setLocalTime(LocalTime.now());
+
         //EU
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/YYYY", Locale.FRENCH);
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm", Locale.FRENCH);
@@ -23,22 +26,18 @@ public class DateServiceImpl implements DateService {
         DateTimeFormatter usaDateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy a");
         DateTimeFormatter usaTimeFormat = DateTimeFormatter.ofPattern("h:mm a");
 
-        dateModel.setLocalDate(LocalDate.now());
-        dateModel.setLocalTime(LocalTime.now());
-
         //EU
         String sDate = dateModel.getLocalDate().format(dateFormatter);
         String sTime = dateModel.getLocalTime().format(timeFormatter);
 
         //US
-        //String uDate = dateModel.getLocalTime().format(usaDateFormat); //Unsupported field: DayOfMonth
+        String uDate = dateModel.getLocalDate().format(usaDateFormat);
         String uTime = dateModel.getLocalTime().format(usaTimeFormat);
-
 
         //EU
         //return " date: \"" + sDate + "\",\ntime:  \"" + sTime + "\"";
 
         //US
-        return " date: \"" + sDate + "\",\ntime:  \"" + uTime + "\"";
+        return " date: \"" + uDate + "\",\ntime:  \"" + uTime + "\"";
     }
 }
