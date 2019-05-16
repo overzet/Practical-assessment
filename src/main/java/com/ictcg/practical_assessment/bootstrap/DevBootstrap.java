f0da32dfac2c1ae4efcffa504bcf6007fba9f2ff
Overzet <overzet@gmail.com>
2019-05-16 10:06:51 +0200

    DevBootstrap added

--- src/main/java/com/ictcg/practical_assessment/bootstrap/DevBootstrap.java
+++ src/main/java/com/ictcg/practical_assessment/bootstrap/DevBootstrap.java
@@ -3,10 +3,13 @@ package com.ictcg.practical_assessment.bootstrap;
 import com.ictcg.practical_assessment.model.Date;
 import com.ictcg.practical_assessment.service.DateService;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+// The purpose of this file is to fill the DB with dummy data each time the app runs
 @Component
+@Profile("dev")// Still needs to be implemented to not be in production jar/war
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private DateService dateService;
@@ -24,8 +27,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private void initData() {
 
         Date eric = new Date("Eric", "Evans");
-
         dateService.save(eric);
+
+        Date rod = new Date("Rod", "Johnson");
+        dateService.save(rod);
+
 
     }
 }
